899ed11bfdb3499db57c7dad9b525a94b5f21e6c
Desai Chen <desaicwtf@gmail.com>
2013-07-02 18:57:47 -0400

    adding struct

--- include/NStructDeclaration.hpp
+++ include/NStructDeclaration.hpp
@@ -9,15 +9,19 @@
 #define NSTRUCTDECLARATION_HPP_
 
 #include "NStatement.hpp"
-
+#include <string>
 class NStructDeclaration: public NStatement
 {
 public:
   NStructDeclaration();
+  NStructDeclaration(const std::string & _name);
   virtual ~NStructDeclaration();
   static NStructDeclaration * FindOrCreate(const NIdentifier & id);
+  void setFields(const VariableList & _fields);
+  std::string getName();
 private:
-  NBlock * body;
+  std::string name;
+  VariableList fields;
 };
 
 #endif /* NSTRUCTDECLARATION_HPP_ */
--- src/NStructDeclaration.cpp
+++ src/NStructDeclaration.cpp
@@ -7,15 +7,45 @@
 
 #include "NStructDeclaration.hpp"
 
-NStructDeclaration::NStructDeclaration():
-body(0)
+std::string
+NStructDeclaration::getName()
 {
-  // TODO Auto-generated constructor stub
-
+  return name;
 }
+
+NStructDeclaration *
+NStructDeclaration::FindOrCreate(const NIdentifier & id)
+{
+  Symbol ss = symbol.findStruct(id.name);
+  if(ss.value == NULL){
+    NStructDeclaration * structNode =new NStructDeclaration(id.name);
+    symbol.addLocalStruct(id.name, structNode );
+    return structNode;
+  }
+  NStructDeclaration * structNode = dynamic_cast<NStructDeclaration*>(ss.value);
+  if(structNode == 0){
+    std::cout<<"Error: Struct symbols contain a non-struct.\n";
+  }
+  return structNode;
+}
+
+void NStructDeclaration::setFields(const VariableList & _fields)
+{
+  //cannot have empty struct body
+  if(fields.size()>0){
+    std::cout<<"Error: struct already defined\n";
+    return;
+  }
+  fields = _fields;
+}
+
+NStructDeclaration::NStructDeclaration(const std::string & _name):
+  name(_name)
+{}
+
+NStructDeclaration::NStructDeclaration()
+{}
 
 NStructDeclaration::~NStructDeclaration()
-{
-  // TODO Auto-generated destructor stub
-}
+{}
 
